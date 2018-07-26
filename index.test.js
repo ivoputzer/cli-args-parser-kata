e46204e4a28d544dd34a70445fff0d2a253c5ac6
Christian Fei <crifei93@gmail.com>
2018-07-27 00:09:12 +0200

    second failing test

--- index.test.js
+++ index.test.js
@@ -5,6 +5,10 @@ test('parses a `simple flag`', () => {
   const args = cliArgsParser(['--foo'])
   assert.deepEqual(args, {foo: true})
 })
+test('parse a `composite` flag', () => {
+  const args = cliArgsParser(['--foo', 'bar'])
+  assert.deepEqual(args, {'foo': 'bar'})
+})
 
 function cliArgsParser () {
   return {foo: true}
